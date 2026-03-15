e01735f122312694537e5275331781592927a306
willCoinCapital <toyorlagos@gmail.com>
2026-03-14 22:20:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,67 @@
-// This will make your dashboard balance change slightly 
-// based on the deposit you enter in the calculator!
-function update(val) {
-    const d = Number(val); 
-    const b = d * 0.5;
-    
-    // Update Calculator
-    document.getElementById('invTxt').innerText = '$' + d.toLocaleString();
-    document.getElementById('bonTxt').innerText = '$' + b.toLocaleString();
-    document.getElementById('totTxt').innerText = '$' + (d + b).toLocaleString();
+const input=document.getElementById("depositInput")
 
-    // Update Dashboard Balance (Simulated)
-    const baseBalance = 45280;
-    document.getElementById('dashBalance').innerText = '$' + (baseBalance + d).toLocaleString();
+if(input){
+
+input.oninput=()=>{
+
+let v=input.value
+
+document.getElementById("invest").innerText="$"+v
+document.getElementById("bonus").innerText="$"+(v0.5)
+document.getElementById("total").innerText="$"+(v1.5)
+
 }
+
+}
+
+const users=["Alpha_User","TraderX","CryptoQueen","Investor88"]
+
+function addLedger(){
+
+const table=document.getElementById("ledgerBody")
+
+if(!table)return
+
+const user=users[Math.floor(Math.random()*users.length)]
+
+const amount="$"+(Math.floor(Math.random()*5000)+500)
+
+const row=document.createElement("tr")
+
+row.innerHTML="<td>"+user+"</td><td>"+amount+"</td><td style='color:#22c55e'>Paid</td>"
+
+table.prepend(row)
+
+if(table.children.length>6) table.lastChild.remove()
+
+}
+
+setInterval(addLedger,4000)
+
+addLedger()
+
+
+
+const investors=["Michael","WhaleTrader","CryptoBoss","AlphaFund"]
+
+function showDeposit(){
+
+const popup=document.getElementById("depositPopup")
+
+const text=document.getElementById("depositText")
+
+if(!popup)return
+
+const name=investors[Math.floor(Math.random()*investors.length)]
+
+const amount="$"+(Math.floor(Math.random()*4000)+300)
+
+text.innerText=name+" deposited "+amount
+
+popup.style.display="block"
+
+setTimeout(()=>popup.style.display="none",4000)
+
+}
+
+setInterval(showDeposit,9000)
